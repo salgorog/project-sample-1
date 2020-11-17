fb5f83594e3bfdc20fc93fc4e6cddf0d847ccaa8
Aztec <salgorog@hotmail.com>
2020-11-16 18:51:41 -0700

    add and clearing individual items functioning

--- js/main.js
+++ js/main.js
@@ -12,22 +12,47 @@ document.addEventListener("readystatechange", (event) => {
 
 const initApp = () => {
     // Add listeners
-    const itemEntryForm =document.getElementById("itemEntryForm");
+    const itemEntryForm = document.getElementById("itemEntryForm");
     itemEntryForm.addEventListener("submit", (event) => {
         event.preventDefault();
         processSubmission();
     });
 
+    const clearItems = document.getElementById("clearItems");
+    clearItems.addEventListener("click", (event) => {
+        const list = toDoList.getList();
+        if (list.length) {
+            const confirmed = confirm(
+                "Are you sure you want to clear the entire list?"
+            );
+            if (confirmed) {
+                toDoList.clearList();
+                updatePersistentData(toDoList.getList());
+                refreshThePage();
+            }
+        }
+    });
+
     // Procedural
-    // load list object
+    loadListObject();
     refreshThePage();
 };
+
+const loadListObject = () => {
+    const storedList = localStorage.getItem("myToDoList");
+    if (typeof storedList !== "string") return;
+    const parsedList = JSON.parse(storedList);
+    parsedList.forEach(itemObj => {
+        const newToDoItem = createNewItem(itemObj._id, itemObj._item);
+        toDoList.addItemToList(newToDoItem);
+    })
+}
 
 const refreshThePage = () => {
     clearListDisplay();
     renderList();
     clearItemEntryField();
-    // setFocusOnItemEntry();
+    setFocusOnItemEntry();
 };
 
 const clearListDisplay = () => {
@@ -70,28 +95,32 @@ const buildListItem = (item) => {
 const addClickListenerToCheckbox = (checkbox) => {
     checkbox.addEventListener("click", (event) => {
         toDoList.removeItemFromList(checkbox.id);
-        // TODO: remove from persistent data
+        updatePersistentData(toDoList.getList());
         setTimeout(() => {
             refreshThePage();
         }, 1000);
     });
 };
 
+const updatePersistentData = (listArray) => {
+    localStorage.setItem("myToDoList", JSON.stringify(listArray));
+}
+
 const clearItemEntryField = () => {
     document.getElementById("newItem").value = "";
 };
 
-const setFocusItemEntry = () => {
+const setFocusOnItemEntry = () => {
     document.getElementById("newItem").focus();
 };
 
 const processSubmission = () => {
     const newEntryText = getNewEntry();
-    if(!newEntryText.length) return;
+    if (!newEntryText.length) return;
     const nextItemId = calcNextItemId();
     const toDoItem = createNewItem(nextItemId, newEntryText);
     toDoList.addItemToList(toDoItem);
-    // TODO: update persistent data 
+    updatePersistentData(toDoList.getList());
     refreshThePage();
 };
 
@@ -102,7 +131,7 @@ const getNewEntry = () => {
 const calcNextItemId = () => {
     let nextItemId = 1;
     const list = toDoList.getList();
-    if (list.length > 0 ) {
+    if (list.length > 0) {
         nextItemId = list[list.length - 1].getId() + 1;
     }
     return nextItemId;
